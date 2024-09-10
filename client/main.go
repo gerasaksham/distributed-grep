31cac938445b019842b7fc82564432c267a8748c
Sanket Donty <sanketdonty@gmail.com>
2024-09-10 17:40:50 -0500

    updated changes: working

--- client/main.go
+++ client/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"math"
+	"net"
+	"net/http"
+	"net/rpc"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 type FileServer struct{}
@@ -19,18 +27,16 @@ type GrepRequest struct {
 	Flag     string
 }
 
-func (fs *FileServer) CreateFile(req FileRequest, reply *string) error {
-	err := os.WriteFile(req.Filename, []byte(req.Content), 0644)
-	if err != nil {
-		return err
-	}
-	*reply = "File created successfully"
-	return nil
-}
+// func (fs *FileServer) CreateFile(req FileRequest, reply *string) error {
+// 	err := os.WriteFile(req.Filename, []byte(req.Content), 0644)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	*reply = "File created successfully"
+// 	return nil
+// }
 
-func (fs *FileServer) GrepFile(req GrepRequest, reply *string) error {
-
-	// cmd2 := exec.Command(req.Command, req.Filename)
+func (fs *FileServer) GrepFile(req *GrepRequest, reply *string) error {
 	cmd2 := exec.Command("grep", req.Flag, req.Pattern, req.Filename)
 	out, err := cmd2.Output()
 	if err != nil {
@@ -41,58 +47,69 @@ func (fs *FileServer) GrepFile(req GrepRequest, reply *string) error {
 }
 
 func main() {
-	var ans string
-	fileServer := new(FileServer)
-	req := GrepRequest{Filename: "test.txt", Pattern: "Hello", Flag: "-i"}
-	fileServer.GrepFile(req, &ans)
-	print(ans)
+	var fileServer FileServer
+	rpc.Register(&fileServer)
+	rpc.HandleHTTP()
 
-	// rpc.Register(fileServer)
-
-	// go func() {
-	// 	l, e := net.Listen("tcp", ":2232")
-	// 	if e != nil {
-	// 		log.Fatal("listen error:", e)
-	// 	}
-	// 	http.Serve(l, nil)
-	// }()
+	go func() {
+		l, e := net.Listen("tcp", ":2232")
+		if e != nil {
+			log.Fatal("listen error:", e)
+		}
+		http.Serve(l, nil)
+	}()
 
 	// filename := "test.txt"
 	// content := "Hello you beautiful human beings from the server"
 
-	// // Create file on the server
+	// Create file on the server
 	// time.Sleep(1 * time.Second)
-	// client, err := rpc.Dial("tcp", "localhost:2233")
-	// if err != nil {
-	// 	log.Fatal("dialing:", err)
-	// }
+
+	const maxRetries = 5
+	const initialDelay = 1 * time.Second
+
+	var client *rpc.Client
+	var err error
+
+	for i := 0; i < maxRetries; i++ {
+		client, err = rpc.DialHTTP("tcp", "localhost:2233")
+		if err == nil {
+			break
+		}
+
+		log.Printf("dialing failed: %v; retrying in %v", err, initialDelay)
+		time.Sleep(time.Duration(math.Pow(2, float64(i))) * initialDelay)
+	}
+
+	if err != nil {
+		log.Fatal("dialing failed after retries:", err)
+	}
 	// defer client.Close()
-	// createReq := FileRequest{Filename: filename, Content: content}
-	// var createReply string
-	// err = client.Call("FileServer.CreateFile", createReq, &createReply)
-	// if err != nil {
-	// 	fmt.Println("Error creating file:", err)
-	// 	return
-	// }
-	// fmt.Println(createReply)
 
-	// reader := bufio.NewReader(os.Stdin)
-	// for {
-	// 	input, _ := reader.ReadString('\n')
-	// 	input = strings.TrimSpace(input)
-	// 	if input != "" {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		fmt.Println("Input:", input)
+		if input != "" {
+			input_split := strings.Split(input, " ")
+			grepCommand := input_split[0]
+			flag := input_split[1]
+			pattern := input_split[2]
+			filename := input_split[3]
+			if grepCommand != "grep" {
+				fmt.Println("Invalid command")
+				continue
+			}
 
-	// 		// Run grep command on the server
-	// 		grepReq := GrepRequest{Filename: filename, Pattern: input}
-	// 		var grepReply string
-	// 		err = client.Call("FileServer.GrepFile", grepReq, &grepReply)
-	// 		if err != nil {
-	// 			fmt.Println("Error running grep:", err)
-	// 			return
-	// 		}
-
-	// 		fmt.Println("Grep result:")
-	// 		fmt.Println(grepReply)
-	// 	}
-	// }
+			grepReq := GrepRequest{Filename: filename, Pattern: pattern, Flag: flag}
+			var grepReply string
+			err = client.Call("FileServer.GrepFile", &grepReq, &grepReply)
+			if err != nil {
+				fmt.Println("Error running grep:", err)
+				return
+			}
+			fmt.Println(grepReply)
+		}
+	}
 }
--- server/main.go
+++ server/main.go
@@ -1,28 +1,18 @@
 package main
 
 import (
-	//"bufio"
 	"bufio"
 	"fmt"
 	"log"
-	"strings"
-	"time"
-
-	//"log"
+	"math"
 	"net"
-	//"net/http"
 	"net/http"
 	"net/rpc"
 	"os"
 	"os/exec"
-	//"strings"
+	"strings"
+	"time"
 )
-
-type Args struct {
-	Str string
-}
-
-type StringArgument struct{}
 
 type FileServer struct{}
 
@@ -34,59 +24,33 @@ type FileRequest struct {
 type GrepRequest struct {
 	Filename string
 	Pattern  string
+	Flag     string
 }
 
-func (fs *FileServer) CreateFile(req FileRequest, reply *string) error {
-	err := os.WriteFile(req.Filename, []byte(req.Content), 0644)
-	if err != nil {
-		return err
-	}
-	*reply = "File created successfully"
-	return nil
-}
-
-func (fs *FileServer) GrepFile(req GrepRequest, reply *string) error {
-	cmd := exec.Command("grep", req.Pattern, req.Filename)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
-	*reply = string(output)
-	return nil
-}
-
-//	func (s *StringArgument) ReturnCapitalizedString(args *Args, reply *string) error {
-//		log.Println("serverfile:", args.Str)
-//		*reply = strings.ToUpper(args.Str)
-//		return nil
-//	}
-// func (s *StringArgument) GrepString(args *Args, reply *string) error {
-// 	// create a new file on the server named "clientfile.txt" and write "Hellop World" to it
-// 	file, err := os.Create("serverfile.txt")
+// func (fs *FileServer) CreateFile(req FileRequest, reply *string) error {
+// 	err := os.WriteFile(req.Filename, []byte(req.Content), 0644)
 // 	if err != nil {
-// 		log.Fatal("error creating file:", err)
+// 		return err
 // 	}
-// 	defer file.Close()
-// 	file.WriteString("Hello World from server")
-// 	// cat the file then grep the string from the file
-// 	cmd := exec.Command("cat", file.Name())
-// 	fmt.Println("cmd:", cmd)
-// 	cmd2 := exec.Command("grep", args.Str)
-// 	cmd2.Stdin, _ = cmd.StdoutPipe()
-// 	fmt.Println("cmd2:", cmd2)
-// 	out, err := cmd2.Output()
-// 	fmt.Println("out:", out)
-// 	if err != nil {
-// 		log.Println("error:", err)
-// 	}
-// 	*reply = string(out)
+// 	*reply = "File created successfully"
 // 	return nil
 // }
 
+func (fs *FileServer) GrepFile(req *GrepRequest, reply *string) error {
+	cmd2 := exec.Command("grep", req.Flag, req.Pattern, req.Filename)
+	out, err := cmd2.Output()
+	if err != nil {
+		log.Println("GrepFile error:", err)
+	}
+	*reply = string(out)
+	return nil
+}
+
 func main() {
-	fileServer := new(FileServer)
-	rpc.Register(fileServer)
+	var fileServer FileServer
+	rpc.Register(&fileServer)
 	rpc.HandleHTTP()
+
 	go func() {
 		l, e := net.Listen("tcp", ":2233")
 		if e != nil {
@@ -94,41 +58,57 @@ func main() {
 		}
 		http.Serve(l, nil)
 	}()
-	// add a timeout here to give the server time to start
-	time.Sleep(1 * time.Second)
-	client, err := rpc.Dial("tcp", "localhost:2232")
-	if err != nil {
-		log.Fatal("dialing:", err)
-	}
-	defer client.Close()
-	filename := "test.txt"
-	content := "Hello you beautiful human beings from the server"
+
+	// filename := "test.txt"
+	// content := "Hello you beautiful human beings from the server"
 
 	// Create file on the server
-	createReq := FileRequest{Filename: filename, Content: content}
-	var createReply string
-	err = client.Call("FileServer.CreateFile", createReq, &createReply)
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
+	// time.Sleep(1 * time.Second)
+
+	const maxRetries = 5
+	const initialDelay = 1 * time.Second
+
+	var client *rpc.Client
+	var err error
+
+	for i := 0; i < maxRetries; i++ {
+		client, err = rpc.DialHTTP("tcp", "localhost:2232")
+		if err == nil {
+			break
+		}
+
+		log.Printf("dialing failed: %v; retrying in %v", err, initialDelay)
+		time.Sleep(time.Duration(math.Pow(2, float64(i))) * initialDelay)
 	}
-	fmt.Println(createReply)
+
+	if err != nil {
+		log.Fatal("dialing failed after retries:", err)
+	}
+	// defer client.Close()
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
+		fmt.Println("Input:", input)
 		if input != "" {
+			input_split := strings.Split(input, " ")
+			grepCommand := input_split[0]
+			flag := input_split[1]
+			pattern := input_split[2]
+			filename := input_split[3]
+			if grepCommand != "grep" {
+				fmt.Println("Invalid command")
+				continue
+			}
 
-			// Run grep command on the server
-			grepReq := GrepRequest{Filename: filename, Pattern: input}
+			grepReq := GrepRequest{Filename: filename, Pattern: pattern, Flag: flag}
 			var grepReply string
-			err = client.Call("FileServer.GrepFile", grepReq, &grepReply)
+			err = client.Call("FileServer.GrepFile", &grepReq, &grepReply)
 			if err != nil {
 				fmt.Println("Error running grep:", err)
 				return
 			}
-
-			fmt.Println("Grep result from client:")
 			fmt.Println(grepReply)
 		}
 	}
